c59741db72c0a99b4d55c69c1f358338097ba251
YousriEstifeda <yousri.essoukri@estifeda.com>
2022-01-22 15:48:56 +0100

    Managing Data

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 import { Product, products} from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,14 +12,24 @@ import { Product, products} from '../products';
 export class ProductDetailsComponent implements OnInit {
 
  product : Product | undefined ;
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { 
+
+    }
 
   ngOnInit(): void {
     //First get the product id from the current route
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
-// Find the product that correspon with the id provided in route
-this.product = products.find(product => product.id === productIdFromRoute);
+    
+    // Find the product that correspon with the id provided in route
+    this.product = products.find(product => product.id === productIdFromRoute);
+    }
+    
+  addToCart(product : Product){
+    this.cartService.addToCart(product);
+    window.alert('Le produit - '+ product.name + ' - a bien été ajouté à votre panier : ' );
   }
-
 }
